313eb0193db8ad29a354442d98b7b72206be064c
moreka <mr.karegar@gmail.com>
2023-11-02 18:04:07 +0330

    game and user creation logic moved to service layer

--- src/main/java/me/moreka/mancala/controller/GamePlayController.java
+++ src/main/java/me/moreka/mancala/controller/GamePlayController.java
@@ -1,17 +1,22 @@
-package com.bol.assignment.controller;
+package me.moreka.mancala.controller;
 
-import com.bol.assignment.dto.*;
-import com.bol.assignment.entity.Game;
-import com.bol.assignment.service.GameFactory;
-import com.bol.assignment.entity.Pit;
-import com.bol.assignment.entity.Player;
-import com.bol.assignment.repository.GameRepository;
-import com.bol.assignment.repository.PitRepository;
-import com.bol.assignment.repository.PlayerRepository;
-import com.bol.assignment.service.GamePlayService;
+import me.moreka.mancala.dto.GameDto;
+import me.moreka.mancala.dto.SelectedPit;
+import me.moreka.mancala.entity.Game;
+import me.moreka.mancala.service.GameService;
+import me.moreka.mancala.entity.Pit;
+import me.moreka.mancala.entity.User;
+import me.moreka.mancala.repository.GameRepository;
+import me.moreka.mancala.repository.PitRepository;
+import me.moreka.mancala.repository.UserRepository;
+import me.moreka.mancala.service.GamePlayService;
+
 import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import me.moreka.mancala.dto.Result;
+import me.moreka.mancala.dto.UsersDto;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,31 +25,21 @@ import org.springframework.web.bind.annotation.*;
 public class GamePlayController {
 
     private final GameRepository gameRepo;
-    private final PlayerRepository userRepo;
+    private final UserRepository userRepo;
     private final PitRepository pitRepo;
-    private final GameFactory gameFactory;
+    private final GameService gameService;
     private final GamePlayService gamePlayService;
 
     @PostMapping("/games/start")
-    public Result<GameDto> start(@RequestBody PlayersDto users) {
-        String username1 = users.getPlayer1().getUsername();
-        var player1 = userRepo.findByUsername(username1);
-        if (player1 == null) {
-            player1 = userRepo.save(new Player(username1));
-        }
-        String username2 = users.getPlayer2().getUsername();
-        var player2 = userRepo.findByUsername(username2);
-        if (player2 == null) {
-            player2 = userRepo.save(new Player(username2));
-        }
-        var game = gameFactory.create(player1, player2);
+    public Result<GameDto> start(@RequestBody UsersDto users) {
+        var game = gameService.create2(users.getPlayer1().getUsername(), users.getPlayer2().getUsername());
         return new Result<>(true, "", GameDto.toDto(game));
     }
 
     @PostMapping("/games/{gameId}/move")
     public Result<GameDto> move(@PathVariable Long gameId, @RequestBody SelectedPit selectedPit) {
         Game game = gameRepo.findById(gameId).orElseThrow();
-        Player player = userRepo.findById(selectedPit.getUserId()).orElseThrow();
+        User player = userRepo.findById(selectedPit.getUserId()).orElseThrow();
         Pit pit =
                 game.getPits().stream().filter(p -> p.getUser().equals(player))
                         .filter(p -> p.getIndex() == selectedPit.getPitIndex()).findFirst().get();
@@ -55,14 +50,14 @@ public class GamePlayController {
         return new Result<>(errorMessage == null, errorMessage, GameDto.toDto(game));
     }
 
-    private String validateMove(Game game, Player player, Pit pit) {
+    private String validateMove(Game game, User player, Pit pit) {
         if (!gamePlayService.isUserTurn(game, player)) {
             return String.format("%s! It's not your turn", player.getUsername());
         }
-        if (!gamePlayService.isUserPit(player, pit)) {
+        if (!pit.isPitOf(player)) {
             return String.format("%s! %s is not your pit.", player.getUsername(), pit.getIndex());
         }
-        if (!gamePlayService.hasEnoughStone(pit)) {
+        if (!pit.hasStone()) {
             return String.format("Pit %s has no stone. Try another pit ;)", pit.getIndex());
         }
         return null;
--- src/main/java/me/moreka/mancala/service/GameService.java
+++ src/main/java/me/moreka/mancala/service/GameService.java
@@ -1,47 +1,56 @@
-package com.bol.assignment.service;
+package me.moreka.mancala.service;
 
 import static java.util.stream.Collectors.*;
 
-import com.bol.assignment.entity.Game;
-import com.bol.assignment.entity.Pit;
-import com.bol.assignment.entity.Player;
-import com.bol.assignment.repository.GameRepository;
-import com.bol.assignment.repository.PitRepository;
+import me.moreka.mancala.entity.Game;
+import me.moreka.mancala.entity.Pit;
+import me.moreka.mancala.entity.User;
+import me.moreka.mancala.repository.GameRepository;
+import me.moreka.mancala.repository.PitRepository;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
+
+import me.moreka.mancala.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class GameFactory {
+public class GameService {
 
     public static int PITS = 6;
     public static int STONES = 6;
     private final GameRepository gameRepo;
     private final PitRepository pitRepo;
+    private final UserRepository userRepository;
+    private final UserService userService;
+    public Game create2(String username1, String username2) {
 
-    public Game create(Player player1, Player player2) {
+        var user1 = userService.findOrCreateUser(username1);
+        var user2 = userService.findOrCreateUser(username2);
 
-        Game game = gameRepo.save(new Game("mancala-" + UUID.randomUUID().getMostSignificantBits(), player1, player2,
+        Game game = gameRepo.save(new Game("mancala-" + UUID.randomUUID().getMostSignificantBits(), user1, user2,
                 new Date()));
         gameRepo.save(game);
+
         List<Pit> pits = new ArrayList<>();
-        List<Pit> player1Pits = createUserPits(game, player1);
-        List<Pit> player2Pits = createUserPits(game, player2);
+        List<Pit> player1Pits = createUserPits(game, user1);
+        List<Pit> player2Pits = createUserPits(game, user2);
         pits.addAll(player1Pits);
         pits.addAll(player2Pits);
         pitRepo.saveAll(pits);
         game.setPits(pits);
-        game.setCurrentPlayer(player1);
+        game.setCurrentPlayer(user1);
         gameRepo.save(game);
         return game;
     }
 
-    private List<Pit> createUserPits(Game game, Player player) {
+
+    private List<Pit> createUserPits(Game game, User player) {
         List<Pit> pits = new ArrayList<>();
         List<Pit> normalPits = IntStream.rangeClosed(0, 5).mapToObj(i -> new Pit(game, player, false, STONES, i))
                 .collect(toList());
