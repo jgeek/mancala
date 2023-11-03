5b563442fe572eda0fc8b0e980e97389840c4518
moreka <mr.karegar@gmail.com>
2023-11-03 12:40:10 +0330

    handle InvalidMoveException

--- src/main/java/me/moreka/mancala/advice/ExceptionHandlerAdvice.java
+++ src/main/java/me/moreka/mancala/advice/ExceptionHandlerAdvice.java
@@ -2,6 +2,7 @@ package me.moreka.mancala.advice;
 
 import me.moreka.mancala.dto.Result;
 import lombok.extern.log4j.Log4j2;
+import me.moreka.mancala.exception.InvalidMoveException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,10 +14,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 class ExceptionHandlerAdvice {
 
     @ResponseBody
+    @ExceptionHandler(InvalidMoveException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Result<Object> invalidMove(InvalidMoveException e) {
+        return new Result<>(false, e.getMessage());
+    }
+
+    @ResponseBody
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     Result<Object> generalError(Exception e) {
         log.error("error: ", e);
-        return new Result<>(false, e.getMessage(), null);
+        return new Result<>(false, e.getMessage());
     }
 }
